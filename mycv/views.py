40bd6b3c562e4f5b87aece27b7038a02c10f7cb7
Lechu Cybulski <cybulski.leszek@gmail.com>
2012-01-18 20:28:01 +0100

    poprawki

--- mycv/views.py
+++ mycv/views.py
@@ -3,7 +3,6 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render_to_response, get_object_or_404
 from cv.mycv.models import vcard, education, experience, course, skill, language, interest
-from reportlab.pdfgen import canvas
 from django.http import HttpResponse
 
 def index(request):
@@ -14,6 +13,8 @@ def index(request):
     skills = skill.objects.all()
     languages = language.objects.all()
     interests = interest.objects.all()
+    
+    vc = vcard.objects.get(pk=1)
   
     return render_to_response('mycv/index.html', locals())
 #    {
@@ -27,23 +28,3 @@ def index(request):
 #        'interests': interests
 #    }
     #)
-    
-def save_as_pdf(request):
-    
-    # Utwórz obiekt HttpResponse z odpowiednimi nagłówkami
-    response = HttpResponse(mimetype='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename=cybulski_leszek_cv.pdf'
-    
-    # Utwórz obiekt PDF, używając obiektu response jako jego "file."
-    p = canvas.Canvas(response)
-    
-    #W tej sekcji dzieje się magia generowania PDF-a
-    p.drawString(100, 100, "Hello world.")
-    
-    #Zamknij obiekt PDF i to będzie wszystko
-    p.showPage()
-    p.save()
-    return render_to_response('mycv/index.html', locals())
-    
-    
-    
