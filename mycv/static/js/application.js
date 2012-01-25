5925514ea5b3819c44504367e17815b50eaa8cb6
Lechu Cybulski <cybulski.leszek@gmail.com>
2012-01-25 10:22:44 +0100

    wylaczenie popup-u przy ladowaniu strony i przeniesienie jego tresci do nowego feature

--- mycv/static/js/application.js
+++ mycv/static/js/application.js
@@ -1,17 +1,31 @@
 $(document).ready(function(){
-  $('body').append('<div class="overlay"></div>');
-  overlay = $('.overlay');
-  overlay.css('opacity','.8');
-  $(".welcome").css('display','block');
-  
-  $(".welcome .close").click(function(){
-    $(this).parent().parent().css('display','none');
-    overlay.remove();
-  });
+  // $('body').append('<div class="overlay"></div>');
+  // overlay = $('.overlay');
+  // overlay.css('opacity','.8');
+  // $(".welcome").css('display','block');
+//   
+  // $(".welcome .close").click(function(){
+    // $(this).parent().parent().css('display','none');
+    // overlay.remove();
+  // });
   
   $('a[rel*=external]').click(function(e) {
     window.open(this.href);
     return false;
   });  
+
+
+  var ajaxLoader = $("#ajax-loader");
+
+  
+  $("#bookmark").toggle(function(){
+    $(ajaxLoader).css('display','block');
+    $("#smenu").slideDown("slow",function() {ajaxLoader.css('display','none')});
+    $(this).animate({top: '+=130'}, 'slow', function(){$(this).css('display','block')});
     
+  }, function(){
+     $(ajaxLoader).css('display','block');
+     $("#smenu").slideUp("slow",function() {$(ajaxLoader).css('display','none')});
+     $(this).animate({top: '-=130'}, 'slow', function(){$(this).css('display','block')});
+   });  
 });
